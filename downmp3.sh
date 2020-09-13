4fcff650085906374dd92dc1c9038df9641a9f98
OT <imot@protonmail.com>
2020-09-13 13:02:24 +0400

    Update downmp3.sh

--- downmp3.sh
+++ downmp3.sh
@@ -4,52 +4,6 @@
 #
 # GLP v3.0 by -07: https://github.com/OTsector
 
-function install {
-	if [[ $1 == "curl" ]]; then
-		apt install curl -y &> /dev/null && echo "curl has been installed"
-	fi
-	if [[ $1 == "youtube-dl" ]]; then
-		curl -s -L -A '' https://yt-dl.org/downloads/latest/youtube-dl \
-			-o /usr/local/bin/youtube-dl && echo "youtube-dl has been installed"
-		chmod a+rx /usr/local/bin/youtube-dl
-	fi
-}
-
-if [ $# -lt 2 ]; then
-	echo -e "\ndownload mp3 by song name...\n"
-	echo "use: "$0" [name of song] [directory location]"
-	echo "example: "$0" \"YTcracker - Antisec\" ~/Desktop"
-	exit 1
-fi
-
-if ! [ -f /usr/bin/curl ]; then
-	echo "ERROR: curl is not installed on this machine"
-	printf "do you whant to install curl? "; read a
-	if [[ $a == "y" ]] || [[ $a == "Y" ]]; then
-		if [ $UID -eq 0 ]; then
-			install curl
-		else
-			echo "ERROR: run this tool with root permission"
-		fi
-	else
-		echo "ERROR: can't process without curl"; exit 1
-	fi
-fi
-if ! [ -f /usr/local/bin/youtube-dl ]; then
-	echo "ERROR: youtube-dl is not installed on this machine"
-	printf "do you whant to install youtube-dl? "; read a
-	if [[ $a == "y" ]] || [[ $a == "Y" ]]; then
-		if [ $UID -eq 0 ]; then
-			install youtube-dl
-		else
-			echo "ERROR: run this tool with root permission"
-		fi
-	else
-		echo "ERROR: can't process without youtube-dl"; exit 1
-	fi
-
-fi
-
 function ascii2url {
 	local text="$*"
 	local out=
@@ -93,4 +47,3 @@ else
 fi
 
 exit 0
-
