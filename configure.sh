28b1dbd011cc4c27b275d6f94ed59b60ebb15635
OT <imot@protonmail.com>
2022-05-07 20:04:55 +0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,10 +14,10 @@ fi
 if ! [ -f /usr/bin/ffplay ]; then
 	apt-get install ffmpeg -y &> /dev/null
 fi
-if ! [ -f /usr/local/bin/youtube-dl ]; then
-	curl -s -L -A '' https://yt-dl.org/downloads/latest/youtube-dl \
-		-o /usr/local/bin/youtube-dl && echo "youtube-dl has been installed"
-	chmod a+rx /usr/local/bin/youtube-dl
+if ! [ -f /usr/local/bin/yt-dlp ]; then
+	curl -s -L -A '' https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp \
+		-o /usr/local/bin/yt-dlp && echo "yt-dlp has been installed"
+	chmod a+rx /usr/local/bin/yt-dlp
 fi
 wget https://raw.githubusercontent.com/OTsector/downmp3/master/downmp3.sh \
 	-O /usr/bin/downmp3 \
